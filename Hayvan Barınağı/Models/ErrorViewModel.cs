a3e7bb179941306866fe4d98802cd146cbfa4db0
tezcanbugra <bugrasabritezcan@gmail.com>
2023-08-05 01:28:31 +0300

    Add project files.

---
+++ Hayvan Barınağı/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hayvan_Barınağı.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
